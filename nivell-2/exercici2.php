ae9f6535d615b9d46232152990b08e43b6368097
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-27 17:24:35 +0100

    refactor: extract average grade calculation into reusable function

--- nivell-2/exercici2.php
+++ nivell-2/exercici2.php
@@ -1,24 +1,27 @@
 <?php
 
-    $studentGrades = [
-        "Anna" => [5, 10, 7, 8, 6],
-        "Bernat" => [3, 6, 9, 4, 7],
-        "Clara" => [7, 8, 6, 9, 10],
-    ];
+$studentGrades = [
+    "Anna" => [5, 10, 7, 8, 6],
+    "Bernat" => [3, 6, 9, 4, 7],
+    "Clara" => [7, 8, 6, 9, 10],
+];
 
-    print_r($studentGrades);
+print_r($studentGrades);
 
-    function calculateAverage($grades) {
-        $total = array_sum($grades);
-        $count = count($grades);
-        return $total / $count;
-    }
+function calculateAverage($grades)
+{
+    $total = array_sum($grades);
+    $count = count($grades);
+    return $total / $count;
+}
 
-    foreach ($studentGrades as $student => $grades) {
-        echo "Average grade for $student: " . calculateAverage($grades) . "\n";
-    }
+foreach ($studentGrades as $student => $grades) {
+    echo "Average grade for $student: " . calculateAverage($grades) . "\n";
+}
 
 
+function calculateClassAverage(array $studentGrades)
+{
     $totalGrades = 0;
     $totalCount = 0;
 
@@ -27,7 +30,10 @@
         $totalCount += count($grades);
     }
 
-    $averageAll = $totalGrades / $totalCount;
-    echo "Average grade for all students: " . $averageAll . "\n";
+    $classAverage = $totalGrades / $totalCount;
+    return $classAverage;
+    
+}
 
-?>
+$classAverage = calculateClassAverage($studentGrades);
+echo "Average grade for all students: " . $classAverage . "\n";
