726cfc907177096c37c3a6ab35465bee2f28ec2c
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-02-27 17:00:47 +0100

    refactor: create array and add numbers is now one single step

--- nivell-1/exercici1.php
+++ nivell-1/exercici1.php
@@ -1,12 +1,6 @@
 <?php
 
-    $array = [];
-
-    $array[] = "1";
-    $array[] = "2";
-    $array[] = "3";
-    $array[] = "4";
-    $array[] = "5";
+    $array = ["1", "2", "3", "4", "5"];
 
     echo implode(", ", $array);
 ?>
